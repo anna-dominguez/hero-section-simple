07f7c1360ca1d0c60235f8d9b7425deaaacbed91
Vesta-nna <anna.dominguez.anna@gmail.com>
2024-10-25 17:18:54 +0200

    add transition and padding on buttons

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => (
 					<h1 className="text-4xl font-semibold sm:text-5xl lg:text-6xl text-neutral-900">
 						Well crafted abstract images
 					</h1>
-					<p className="text-lg sm:text-xl lg:text-xl text-neutral-600">
+					<p className="text-lg sm:text-xl text-neutral-600">
 						High quality abstract images for your projects, wallpaper and
 						presentations.
 					</p>
@@ -14,13 +14,13 @@ const App = () => (
 				<div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:flex sm:gap-8">
 					<button
 						type="button"
-						className="px-5 py-3 text-base bg-white border rounded shadow sm:text-lg sm:px-6 sm:py-4 border-neutral-200 text-neutral-900 h-fit focus:outline focus:outline-indigo-100 hover:bg-neutral-50 focus:bg-neutral-50"
+						className="px-5 py-3 text-base transition-all duration-300 ease-in-out bg-white border rounded shadow sm:text-lg sm:px-10 sm:py-4 border-neutral-200 text-neutral-900 h-fit focus:outline focus:outline-indigo-100 hover:bg-neutral-50 focus:bg-neutral-50"
 					>
 						Learn more
 					</button>
 					<button
 						type="button"
-						className="px-5 py-3 text-base text-white bg-indigo-700 rounded shadow sm:text-lg sm:px-6 sm:py-4 hover:bg-indigo-800 outline-4 outline-indigo-100 focus:outline focus:outline-indigo-100 h-fit focus:bg-indigo-800"
+						className="px-5 py-3 text-base text-white transition duration-300 ease-in-out bg-indigo-700 rounded shadow sm:text-lg sm:px-10 sm:py-4 hover:bg-indigo-800 outline-4 outline-indigo-100 focus:outline focus:outline-indigo-100 h-fit focus:bg-indigo-800"
 					>
 						See pricing
 					</button>
